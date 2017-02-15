d05e2af4c7df8d3cec88f93cb01eeb9e3966f941
Bruce Prewit <bprewit@gmail.com>
2017-02-15 13:47:20 -0600

    Debugging

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// vim:ts=4 background=light
 #include "mbed.h"
 #include "ArduinoMotorShield.hpp"
 
@@ -11,9 +12,9 @@
 typedef int(*CMD_PTR_T)(const char *);
 
 typedef struct {
-    char cmd_name[CMD_NAME_LEN];
-    CMD_PTR_T cmd;
-    char help[CMD_HELP_LEN];
+	char cmd_name[CMD_NAME_LEN];
+	CMD_PTR_T cmd;
+	char help[CMD_HELP_LEN];
 } CMD_TABLE_ENTRY;
 
 /****************************************************************************
@@ -21,23 +22,23 @@ typedef struct {
  ***************************************************************************/
 typedef enum 
 {
-    MOTOR_FWD, 
-    MOTOR_REV,
+	MOTOR_FWD, 
+	MOTOR_REV,
 } MOTOR_DIRECTION;
 
 typedef enum
 {
-    MOTOR_STOPPED,
-    MOTOR_STARTING,
-    MOTOR_SPEEDUP,
-    MOTOR_RUNNING,
-    MOTOR_SLOWDOWN,
+	MOTOR_STOPPED,
+	MOTOR_STARTING,
+	MOTOR_SPEEDUP,
+	MOTOR_RUNNING,
+	MOTOR_SLOWDOWN,
 } MOTOR_STATE;
 
 typedef enum
 {
-    LED_OFF = 0,
-    LED_ON  = 1
+	LED_OFF = 0,
+	LED_ON  = 1
 } LED_STATE;
 
 /****************************************************************************
@@ -56,7 +57,8 @@ ArduinoMotorShield	ams;
 /****************************************************************************
  ** function prototypes 
  ****************************************************************************/
-void led_blink(void);
+void led2_blink(void);
+void led1_blink(void);
 void counter_read_reset(void);
 void led_reset(void);
 
@@ -76,11 +78,11 @@ int set_direction(const char *);
  ***************************************************************************/
 CMD_TABLE_ENTRY cmd_table[] = 
 {
-    {"pwr?",	&get_pw,	"Print motor power"		},
-    {"pwr",	&set_pw,	"Set motor power"		},
-    {"dir?",	&get_direction,	"Print motor direction",	},
-    {"dir",	&set_direction,	"Set motor direction",		},
-    {"help",	NULL, 		"Print some nice help"		},
+	{"pwr?",	&get_pw,		"Print motor power"			},
+	{"pwr",		&set_pw,		"Set motor power"			},
+	{"dir?",	&get_direction,	"Print motor direction",	},
+	{"dir",		&set_direction,	"Set motor direction",		},
+	{"help",	NULL, 			"Print some nice help"		},
 };
 
 const int n_cmds = sizeof(cmd_table)/sizeof(CMD_TABLE_ENTRY);
@@ -107,113 +109,115 @@ int main()
 
     led1 = LED_ON;
 
-    timer.start();
-    event.rise(counter_read_reset);
-    event.fall(led_reset);
+    //timer.start();
+    //event.rise(counter_read_reset);
+    //event.fall(led_reset);
 
-    sp.baud(115200);
+    sp.baud(9600);
     sp.printf("AVC Test Device Ready\n");
+
+	ticker.attach(led1_blink, 250);
 
     ams.SetMotorPolarity(ArduinoMotorShield::MOTOR_A, ArduinoMotorShield::MOTOR_FORWARD);
     ams.SetMotorPower(ArduinoMotorShield::MOTOR_A, motor_power);
 
-    while(true) 
-    {
-	if(gets(rx_buf) != NULL)
+	while(true) 
 	{
-	    int fields = sscanf(rx_buf, "%s %s", cmd, arg);
-	    for(int i = 0; i < n_cmds; ++i)
-	    {
-		if(strncmp(cmd, cmd_table[i].cmd_name, strlen(cmd_table[i].cmd_name)) == 0)
+		if(gets(rx_buf) != NULL)
 		{
-		    cmd_table[i].cmd(arg);
-		    break;
+			int fields = sscanf(rx_buf, "%s %s", cmd, arg);
+			for(int i = 0; i < n_cmds; ++i)
+			{
+				if(strncmp(cmd, cmd_table[i].cmd_name, strlen(cmd_table[i].cmd_name)) == 0)
+				{
+					cmd_table[i].cmd(arg);
+					break;
+				}
+			}
+			memset(rx_buf, 0, sizeof(rx_buf));
+			fields = 0;
 		}
-	    }
-	    memset(rx_buf, 0, sizeof(rx_buf));
-	    fields = 0;
 	}
-    }
 }
 
 
 /*
-** fetch pulse width
-*/
+ ** fetch pulse width
+ */
 int get_pw(const char *)
 {
-    printf("pwr = %d\n", motor_power);
-    return(0);
+	printf("pwr = %d\n", motor_power);
+	return(0);
 }
 
 /*
-** set pulse width (motor power)
-*/
+ ** set pulse width (motor power)
+ */
 int set_pw(const char *arg)
 {
-    int pw_val = 0;
-    pw_val = atoi(arg);
-    printf("setting pwr = %d\n", pw_val);
-    motor_power = pw_val;
-    ams.SetMotorPower(ArduinoMotorShield::MOTOR_A, (motor_power/100.0f));
-    return(0);
+	int pw_val = 0;
+	pw_val = atoi(arg);
+	printf("setting pwr = %d\n", pw_val);
+	motor_power = pw_val;
+	ams.SetMotorPower(ArduinoMotorShield::MOTOR_A, (motor_power/100.0f));
+	return(0);
 }
 
 /*
-** fetch & print motor direction
-*/
+ ** fetch & print motor direction
+ */
 int get_direction(const char *arg)
 {
-    char *msg;
+	char *msg;
 
-    switch(motor_dir)
-    {
-    case MOTOR_FWD:
-	msg = "fwd";
-	break;
-    case MOTOR_REV:
-	msg = "rev";
-	break;
-    default:
-	msg = "invalid";
-	break;
-    }
-    printf("dir = %s\n", msg);
-    return(0);
+	switch(motor_dir)
+	{
+		case MOTOR_FWD:
+			msg = "fwd";
+			break;
+		case MOTOR_REV:
+			msg = "rev";
+			break;
+		default:
+			msg = "invalid";
+			break;
+	}
+	printf("dir = %s\n", msg);
+	return(0);
 }
 
 
 /*
-** set motor direction
-*/
+ ** set motor direction
+ */
 int set_direction(const char *arg)
 {
-    MOTOR_DIRECTION dir;
+	MOTOR_DIRECTION dir;
 
-    if(strcmp(arg, "fwd") == 0)
-    {
-	dir = MOTOR_FWD;
-    }
-    else if(strcmp(arg, "rev") == 0)
-    {
-	dir = MOTOR_REV;
-    }
-    else
-    {
-	return(-1);
-    }
+	if(strcmp(arg, "fwd") == 0)
+	{
+		dir = MOTOR_FWD;
+	}
+	else if(strcmp(arg, "rev") == 0)
+	{
+		dir = MOTOR_REV;
+	}
+	else
+	{
+		return(-1);
+	}
 
-    if(motor_dir != dir)
-    {
-	/* changing direction => stop motor first */
-	ams.SetMotorPower(ArduinoMotorShield::MOTOR_A, 0.0f);
-    }
-    /* set motor to new direction */
-    motor_dir = dir;
-    ams.SetMotorPolarity(ArduinoMotorShield::MOTOR_A, (ArduinoMotorShield::MotorDirection_e)dir);
-    ams.SetMotorPower(ArduinoMotorShield::MOTOR_A, (motor_power / 100.0f));
-    printf("Setting motor direction to %d\n", dir);
-    return(0);
+	if(motor_dir != dir)
+	{
+		/* changing direction => stop motor first */
+		ams.SetMotorPower(ArduinoMotorShield::MOTOR_A, 0.0f);
+	}
+	/* set motor to new direction */
+	motor_dir = dir;
+	ams.SetMotorPolarity(ArduinoMotorShield::MOTOR_A, (ArduinoMotorShield::MotorDirection_e)dir);
+	ams.SetMotorPower(ArduinoMotorShield::MOTOR_A, (motor_power / 100.0f));
+	printf("Setting motor direction to %d\n", dir);
+	return(0);
 }
 
 /*
@@ -233,4 +237,14 @@ void counter_read_reset(void)
 void led_reset(void)
 {
     led1 = LED_OFF;
+}
+
+void led1_blink(void)
+{
+	led1 = !led1;
+}
+
+void led2_blink(void)
+{
+	led2 = !led2;
 }
